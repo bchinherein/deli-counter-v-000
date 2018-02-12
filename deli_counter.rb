fe210d1092d9fb3b2a167f566e4dded3a39da0f3
Brandon Chin <brandonchin@me.com>
2018-02-12 03:21:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   else
     phrase = "The line is currently:"
     katz_deli.each_with_index do |person, index|
-    phrase_array.push(index, person)
+    phrase.push"#{index + 1}. #{person}"
 
     end
     puts phrase
