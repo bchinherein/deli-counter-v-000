3edabc58dffe05070199a5259b0c42a45a7fc4e5
Brandon Chin <brandonchin@me.com>
2018-02-12 03:04:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    phrase_array = []
+    katz_deli.each_with_index do |person, index|
+    phrase_array = [index, person]
+
+    end
+    puts "The line is currently:" + phrase_array.join
+
+  end
+end
+
+#Need to push #index and #person to phrase.
+#Index and person will need to be an array that is .join
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
